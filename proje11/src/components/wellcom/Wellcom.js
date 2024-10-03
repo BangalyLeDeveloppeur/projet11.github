ee6015115e909538a805627efd25c779a168beac
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2024-10-03 09:34:38 +0200

    styles du composant wellcom

--- proje11/src/components/wellcom/Wellcom.js
+++ proje11/src/components/wellcom/Wellcom.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Wellcom = () => {
-    return (
-        <div>
-            <div class="header">
-        <h1>Welcome back<br />Tony Jarvis!</h1>
+  return (
+    <div className="wellcom">
+      <div class="wellcom-header">
+        <h1>
+          Welcome back
+          <br />
+          Tony Jarvis!
+        </h1>
         <button class="edit-button">Edit Name</button>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Wellcom;
