e959f32481f137f9a7bfca19f3387153bea0444d
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2024-10-03 11:06:48 +0200

    travailler sur la section tranjaction

--- proje11/src/pages/Tranjaction.js
+++ proje11/src/pages/Tranjaction.js
@@ -8,6 +8,8 @@ const Tranjaction = () => {
     <div>
       <Wellcom />
       <Account title={"Argent Bank Checking (x8349)"} solde={"$2,082.79"} content={"Available Balance"}/>
+      <Account title={"Argent Bank Savings (x6712)"} solde={"$10,928.42"} content={"Available Balance"}/>
+      <Account title={"Argent Bank Credit Card (x8349)"} solde={"$184.30"} content={"Available Balance"}/>
     </div>
   );
 };
